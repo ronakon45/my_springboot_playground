df64f26d92d95c206f567d921eaabd8ac5651da3
RONAKON <ronakon.wat@axonstech.com>
2023-05-23 10:21:04 +0700

    Post Customer

--- src/main/java/com/ronakon45/playground/CustomerController.java
+++ src/main/java/com/ronakon45/playground/CustomerController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class CustomerController {
     @GetMapping
     List<Customer> all() {
       return (List<Customer>) repository.findAll();
+    }
+
+    @PostMapping
+    Customer newCustomer(@RequestBody Customer newCustomer) {
+      return repository.save(newCustomer);
     }
 
 
